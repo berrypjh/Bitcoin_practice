1189f9b0d5a75432147b821d60f52f32a0154387
Junhyuk Park <junhyeg08@gmail.com>
2023-04-24 23:58:02 +0900

    feat: 블록체인 ws (웹소켓) 서버 구축

--- httpServer.js
+++ httpServer.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { getBlocks, getVersion, nextBlock, addBlock } = require("./block");
+const { initP2PServer, connectToPeers, getSockets } = require("./p2pServer");
 
 const HTTP_PORT = process.env.HTTP_PORT || 4000;
+const P2P_PORT = process.env.P2P_PORT || 6000;
 
 const initHttpServer = () => {
     const app = express();
@@ -28,9 +30,24 @@ const initHttpServer = () => {
         process.exit();
     });
 
+    app.post("/addPeers", (req, res) => {
+        const peer = req.body.peer || [];
+        connectToPeers(peer);
+        res.send("success");
+    });
+
+    app.get("/peers", (req, res) => {
+        let sockInfo = [];
+        getSockets().forEach((s) => {
+            sockInfo.push(s._socket.remoteAddress + ":" + s._socket.remotePort);
+        });
+        res.status(200).json({ peer: sockInfo, success: true });
+    })
+
     app.listen(HTTP_PORT, () => {
         console.log(`Listening Http Port : ${HTTP_PORT}`);
     });
 };
 
 initHttpServer();
+initP2PServer(P2P_PORT);
